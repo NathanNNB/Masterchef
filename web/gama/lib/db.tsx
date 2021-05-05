9c82cf6f9f8dc67173615962e94a31e965ca1797
NathanNNB <nathan-borges2011@live.com>
2021-05-05 13:46:32 -0300

    Att
    att db

--- web/gama/lib/db.tsx
+++ web/gama/lib/db.tsx
@@ -1,10 +1,8 @@
 import {MongoClient} from 'mongodb'
-const user = process.env.NAME_DATABASE;
-const pwd = process.env.PWD_DATABASE;
-const namedb = process.env.NAME_DATABASE;
-export async function connectToDatabase(){
+  
   const client = await MongoClient.connect(
-    `mongodb+srv://${user}:${pwd}@teste.h0urp.mongodb.net/${namedb}?retryWrites=true&w=majority`
+    'mongodb+srv://dbUser:IoCYwlCgxOXY1i7j@teste.h0urp.mongodb.net/teste0?retryWrites=true&w=majority'
   );
+    
     return client;
   };
