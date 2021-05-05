1664aa608a6739093159dee333183442f7176e0b
NathanNNB <nathan-borges2011@live.com>
2021-05-05 11:43:46 -0300

    UPDATE lib db
    Adicionando variáveis de ambiente

--- web/gama/lib/db.tsx
+++ web/gama/lib/db.tsx
@@ -1,8 +1,10 @@
 import {MongoClient} from 'mongodb'
-
+const user = process.env.NAME_DATABASE;
+const pwd = process.env.PWD_DATABASE;
+const namedb = process.env.NAME_DATABASE;
 export async function connectToDatabase(){
   const client = await MongoClient.connect(
-    'mongodb+srv://dbUser:QEw1paaa5tdvt6VP@teste.h0urp.mongodb.net/teste0?retryWrites=true&w=majority'
+    `mongodb+srv://${user}:${pwd}@teste.h0urp.mongodb.net/${namedb}?retryWrites=true&w=majority`
   );
     return client;
   };
