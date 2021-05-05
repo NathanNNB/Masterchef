1120a34bb3039bc61a9a94dc07c7d9c8aa6e18a2
NathanNNB <nathan-borges2011@live.com>
2021-05-05 13:50:11 -0300

    Att
    att

--- web/gama/lib/db.tsx
+++ web/gama/lib/db.tsx
@@ -1,4 +1,6 @@
 import {MongoClient} from 'mongodb'
+
+export async function connectToDatabase(){
   
   const client = await MongoClient.connect(
     'mongodb+srv://dbUser:IoCYwlCgxOXY1i7j@teste.h0urp.mongodb.net/teste0?retryWrites=true&w=majority'
