4ab0a1daf3051b509a437b2f2edc45c518a51770
NathanNNB <nathan-borges2011@live.com>
2021-05-07 10:22:27 -0300

    Commit 5
    Adicionando Review/Profile/useHeader (Responsivo)

--- web/gama/components/profile/user-profile.tsx
+++ web/gama/components/profile/user-profile.tsx
@@ -35,7 +35,7 @@ const Profile: React.FC<profileProps> = (
           </span>
           
         </div>
-        <div>
+        <div className={styles.friendsIcon}>
           <Link href="/profile/user">
           <a>
             <FaUserFriends className={styles.userFriendsIcon}/>
@@ -46,6 +46,17 @@ const Profile: React.FC<profileProps> = (
       </div>
 
       <div className={styles.reviews}>
+        <Review
+        authorImage="https://i.pinimg.com/originals/79/a6/f2/79a6f25706fe05e38089e08239044eb5.jpg"
+        authorName="Tim Maia"
+        reviewRate= {8.5}
+        itemImage="https://veja.abril.com.br/wp-content/uploads/2019/09/blogib_lotr-a-sociedade-do-anel_feat.jpg?quality=70&strip=info&w=680&h=453&crop=1"
+        itemTitle="O Senhor dos Anéis: A Irmandade do Anel"
+        reviewText="Excellent well-made adventure film especially when compared with mediocre output 
+        of current superhero movies. Grand old fashioned entertainment."
+        likeNumber= {45}
+        />
+
         <Review
         authorImage="https://i.pinimg.com/originals/79/a6/f2/79a6f25706fe05e38089e08239044eb5.jpg"
         authorName="Tim Maia"
